4a95e6fdc607386cf14f47358b291114b61f7d8f
Tobias Kiertscher <dev@mastersign.de>
2022-02-18 18:24:54 +0000

    switched to AvalonEdit (WPF) started developing YAML syntax grammar

---
+++ src/Mastersign.AutoForm/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Highlighting;
+
+namespace Mastersign.AutoForm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void InitializeHighlighting()
+        {
+            var t = typeof(MainWindow);
+            var a = t.Assembly;
+            using (var s = a.GetManifestResourceStream(t.Namespace + ".YAML-Mode.xshd"))
+            {
+                using (var r = new XmlTextReader(s))
+                {
+                    textEditor.SyntaxHighlighting = HighlightingLoader.Load(r, HighlightingManager.Instance);
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeHighlighting();
+        }
+    }
+}
