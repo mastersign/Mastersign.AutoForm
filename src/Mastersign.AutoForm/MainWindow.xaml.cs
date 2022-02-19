aa5d04ab7d8e80889a6758662c6b0664c256dfe3
Tobias Kiertscher <dev@mastersign.de>
2022-02-19 08:15:54 +0000

    created imperfect but usable YAML syntax highlighting grammar

--- src/Mastersign.AutoForm/MainWindow.xaml.cs
+++ src/Mastersign.AutoForm/MainWindow.xaml.cs
@@ -40,6 +40,49 @@ namespace Mastersign.AutoForm
         {
             InitializeComponent();
             InitializeHighlighting();
+            textEditor.Text = EXAMPLE;
         }
+
+        private const string EXAMPLE = @"# This is a YAML document
+title: Dark Theme Demo
+theme: 'dark'
+grid:
+  # comments
+  columns: 4
+  rows:5
+'defaultSlot': main # comments
+array:
+- 'abc'
+- ABC
+- true
+- 123
+- 127.0.0.1
+slots:
+  main: { columnSpan: 4, rowSpan: '3','history': ""20}"", ""extensions"":50 } # CMT
+  A:
+    visible: yes
+    row: 3
+    ""column Span"":'wide'
+    'rowSpan': null
+    colorFilter: saturate(30%) contrast(400%) brightness(50%)
+  B: {
+    row: 3,
+    column: 'auto',
+    columnSpan: 2, # CMT
+    rowSpan: 2,
+    extensions: [ ""a{"", 'b', {x: 123}, ]
+    extensions2: [""a,"",'b',{'x':123},]
+  }
+docstring: |
+  ABC
+  DEF
+
+'something else': >+3
+   This
+   https://boomack.com
+   is more text.
+
+# THE END
+";
     }
 }
